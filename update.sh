047013a274bd00c0e1e68d10f97db3ab44b988b5
Nilberto César <nilbertocs@gmail.com>
2019-10-10 01:35:11 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ read -p "Start installation? (Y/N)? " -n 1 -r
 echo
 case "$REPLY" in 
   y|Y )
+  tput setaf 4 ; tput bold ;echo "Starting..."; tput sgr0;
     rm -r /home/xtreamcodes/iptv_xtream_codes/admin/*
     wget https://github.com/Nilbertocs/XtreamUI/archive/master.zip -O /tmp/update.zip -o /dev/null
     unzip /tmp/update.zip -d /tmp/update/
@@ -12,8 +13,9 @@ case "$REPLY" in
     chmod -R 0777 /home/xtreamcodes/iptv_xtream_codes/admin
     chown -R xtreamcodes:xtreamcodes /home/xtreamcodes/iptv_xtream_codes/admin
     rm /home/xtreamcodes/iptv_xtream_codes/admin/README.md
-n|N ) echo "Stop Installation ..."; tput sgr0; ;;
-* ) echo "Type only Y or N";tput sgr0; ;;
+    tput setaf 2 ; tput bold ;echo "..."; tput sgr0
+n|N ) tput setaf 1 ; tput bold ;echo  "Stop Installation ..."; tput sgr0; ;;
+* ) tput setaf 1 ; tput bold ; echo "Type only Y or N";tput sgr0; ;;
 esac
 rm update.sh
 exit 0
